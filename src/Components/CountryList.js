f96a8c9d0159b0d85e65c00dd12bc70984ab2b83
ipe <iippol@protonmail.com>
2021-06-18 13:27:19 +0300

    ADD CountryList now also maps flag image

--- src/Components/CountryList.js
+++ src/Components/CountryList.js
@@ -3,11 +3,12 @@ import React from "react";
 const CountryList = ({ countryList = [] }) => {
     return (
         <>
-            {countryList.map((data, index) => {
+            {countryList.map((data) => {
                 if (data) {
                     return (
                         <div key={data.name}>
                             <h1>{data.name}</h1>
+                            <img src={data.flag} alt="flag" width="200px" />
                         </div>
                     );
                 }
