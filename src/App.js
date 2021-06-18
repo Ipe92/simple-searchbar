dec5db654aa12faa459aaab70657a29aca850591
ipe <iippol@protonmail.com>
2021-06-18 13:11:41 +0300

    Add CountryList, SearchBar, SearchPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import "./App.css";
+import SearchPage from "./Components/SearchPage.js";
 
 function App() {
     return (
         <div className="App">
-            <p>simple searchbar</p>
+            <SearchPage />
         </div>
     );
 }
